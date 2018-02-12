b598b3d57bde3bc26cb56dd7fd9da0844e1591e9
Bessie Arino <brarino@gmail.com>
2018-02-12 15:12:38 -0800

    completed lab

---
+++ lab-bessie/lib/greet.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(name) {
+    if(typeof name !== 'string') {
+        return null;
+    };
+    return `hello ${name}`;
+};
---
+++ lab-bessie/test/greet-test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const greet = require('../lib/greet.js');
+const assert = require('assert');
+
+describe('Greet Module', function() {
+    describe('#greet', function() {
+        it('should say hello world', function() {
+            var result = greet('world');
+            assert.ok(result === 'hello world', 'not equal to hello world');
+        });
+        it('should say null', function(){
+            var result = greet(88);
+            assert.ok(result === null, 'result not null');
+        });
+    });
+});

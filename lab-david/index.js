bbb1e9cde9f66a816b267f6236566092a8c5454d
David Kosmos <dakos@Davids-MacBook-Pro.local>
2018-02-12 16:37:04 -0800

    cleaned up index.js and added Readme

--- lab-david/index.js
+++ lab-david/index.js
@@ -2,10 +2,3 @@
 
 const greet = require('./lib/greet.js');
 const arithmetic = require('./lib/arithmetic.js');
-
-greet(2348);
-greet('David');
-arithmetic.add(10,10);
-arithmetic.add('fool', 10);
-arithmetic.sub(10, 9);
-arithmetic.sub('fool', 9);

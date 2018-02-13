3e1c0bbd655b14d450f3d3087d5dcb6699b3a020
tamarushin <tamarushin@gmail.com>
2018-02-12 22:17:54 -0800

    arithmatic test not even close yet

--- lab-tama/lib/arithmetic.js
+++ lab-tama/lib/arithmetic.js
@@ -1,1 +1,8 @@
 'use strict';
+
+module.exports = exports = {};
+
+exports.add = function(number){
+  if (!number) throw new Error ('not a number');
+  return `hey ${number}`;
+};
---
+++ lab-tama/test/arithmatic-test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const arithmatic = require('../lib/arithmatic.js');
+const assert = require('assert');
+
+describe('Arithmatic Module'), function() {
+    describe('#add', function(){
+  it('it should return the sum of two numbers'), function() {
+    var result = arithmatic.add(5,8);
+    assert.ok(result === 13, 'not equal to 13');
+    };
+
+    it('should throw a non-number error', function(){
+        assert.throws(function() {
+            mathematics.add();
+        }, 'error not thrown');
+    });
+  });
+
+
+
+// The add method should have a 2 parameters(airty of two)
+// if either argument is a non - number the function should return null
+// else return the sum of the 2 numbers
